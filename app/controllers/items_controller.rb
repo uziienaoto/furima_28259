5099b3c68c3a1fd52830980730e62c9c2701eac2
uziienaoto <otoaneiizu@gmail.com>
2020-08-11 08:54:37 +0900

    ログインしているユーザーだけが、出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,14 @@ class ItemsController < ApplicationController
 
   def index
   end
+  
+  def new
+  end
 
+  def create
+
+  end
+  
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
